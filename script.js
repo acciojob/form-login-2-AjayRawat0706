fab9c1899d2131502fda7292c2477038312db9d8
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-18 21:31:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function fun() {
+	let firstName = document.getElementsByName("First Name")[0].value;
+	let lastName=document.getElementsByName("Last Name")[0].value;
+	let num=document.getElementsByName("Phone Number")[0].value;
+	let mail=document.getElementsByName("Email ID")[0].value;
+	
+	alert(`First Name: ${firstName} Last Name: ${lastName} Number: ${num} Email ID: ${mail}`);
+}
